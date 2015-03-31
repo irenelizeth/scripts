a37aaaafec5f38f79f346f44e272e8fefd7d611d
irenelizeth <imanotas@udel.edu>
2015-03-31 16:42:02 -0400

    adding useful comments

--- bash_scripts/count_appversions.sh
+++ bash_scripts/count_appversions.sh
@@ -2,19 +2,19 @@
 
 if [ -z "$1" ]; then
 	echo first arg is empty 
-	echo usage: $0 directory matching_string num_sites
+	echo usage: $0 directory matching_string num_sites app_name inter
 	exit
 fi
 
 if [ -z "$2" ]; then 
         echo second arg is empty 
-        echo usage: $0 directory matching_string num_sites
+        echo usage: $0 directory matching_string num_sites app_name inter
         exit
 fi
 
 if [ -z "$3" ]; then 
         echo third arg is empty 
-        echo usage: $0 directory matching_string num_sites
+        echo usage: $0 directory matching_string num_sites app_name inter
         exit
 fi
 
@@ -22,16 +22,18 @@ DIRECTORY=$1
 MATCH_STRING=$2
 TSITES=$3
 
+# arg $4 is used to indicate name of app subject
 if [ -z "$4" ]; then
 	APP_NAME=$MATCH_STRING
 else
 	APP_NAME=$4
 fi
 
+# arg $5 is used to indicate if script should be interactive and analyze subdirectories
 if [ -z "$5" ]; then
 	ITER=false
 else
-	ITER=true
+	ITER=true # any value for $5 indicates it should be interactive
 fi
 
 function calculate_total_persite() {
@@ -63,6 +65,7 @@ if [ "$ITER" = "true" ]; then
 	done		
 else
 	echo total files in $DIRECTORY : $(ls -1 $DIRECTORY | wc -l) >> results-$APP_NAME.txt
+	#TO-DO: test when non-interactive
         calculate_total_persite $DIRECTORY
 fi
 
