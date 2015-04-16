8e509314ecc5a307414b1b7404df89f5ad24f061
irenelizeth <imanotas@udel.edu>
2015-04-16 10:22:33 -0400

    cleaning script

--- count_implementations.R
+++ count_implementations.R
@@ -6,16 +6,6 @@
 # file: path to data file containing results of impplementations per site
 # format input file: <row> : site#,impl1, impl2, impl3,...,impln
 
-
-#OptionParser(usage = "usage: %prog [file] ",
-#    option_list <- list(
-#        make_option("--file", action="store_true", default=FALSE,
-#        help="Path to file with implementation detatils per site [default]"),
-#        make_option(c("-p", "--top percentage"), type="integer", default=100,
-#        help="defines percentage of top implementations to consider [default %default]")
-#    ),
-#    add_help_option = TRUE, prog = "count_implementations.R", description = "Prints site #, # implementations, # top implementations", epilogue = "")
-
 args <- commandArgs(TRUE)
 
 file = args[1]
@@ -29,7 +19,6 @@ if(top_par<=0)
 
 cat(paste("subject, ","site, ","total, ","top","\n", sep=""))
 
-# freqCollections.csv
 #read top alternatives file and create a set of top alternatives
 top_list = read.csv("freqCollections.csv")
 
@@ -43,8 +32,6 @@ data = read.csv(file, stringsAsFactors=FALSE, skipNul=TRUE, na.strings="", heade
 # analyze how many in top list
 for (item in 1:nrow(data)){
     
-    #cat(paste("row ",item, "\n",sep=""))
-    
     #site number
     nSite = data[item, 1]
     
@@ -53,19 +40,16 @@ for (item in 1:nrow(data)){
     
     list <- list()
     # restructure implementation names from data
-    
     if(nImpl>0){
         for(i in 2:(nImpl+1)){
-            # get the name of the implementation from the string
             str = unlist(strsplit(data[item,i],split=data[item,1],fixed=TRUE))
+            # get subject's name
             subject = unlist(strsplit(str[1],split="alternatives",fixed=TRUE))[1]
             subject_name <- substr(subject,1,nchar(subject)-1)
+            # get the name of the implementation from the string
             list[i-1] <- substr(str[2],2,nchar(str[2])-4)
         }
     }
-    
-    
-    #cat(paste("######### site ",data[item,1],"\n", sep=""))
     
     count = 0
     for(i in 1:length(topM)){
@@ -84,7 +68,6 @@ for (item in 1:nrow(data)){
     
     # which top implementations are included?
     nTop = length(which(res==TRUE,)) # return indices of implementations in topM
-    #print site #, # total implemen. , # implem. top list
     cat(paste(subject_name, ",", nSite, ", ",nImpl, ", ", count, "\n", sep=""))
     
 }
