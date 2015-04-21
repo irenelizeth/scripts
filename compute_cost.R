37b4a05b63bff319f75c259e5631743b199b132a
irenelizeth <imanotas@udel.edu>
2015-04-21 15:09:49 -0400

    refactor compute_cost function to be used by two strategies (alternatives, hotspots)

--- compute_cost.R
+++ compute_cost.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
-# data files format:
-# subject_name, site, total, top.35
+# format's input file :
+# subject's name, site no., total implem., top 100 energy-efficient(ee), top 90 ee, ..., top 10 ee
+# subject_name, site, total, top.100, top.90, ..., top.10
 
 args <-commandArgs(TRUE)
 
@@ -21,30 +22,25 @@ repetitions <- c(10, 10, 3, 10, 10, 5)
 
 setwd(args[1])
 
-# print cost of choosing different percentages of energy efficient alternatives:
-cat(paste("subject, ", "cAll, ", "c100, ", "c90, ", "c80, ", "c70, ", "c60, ", "c50, ", "c40, ", "c30, ", "c20, ", "c10", "\n", sep=""))
-
-for(item in list.files()){
+compute_cost <- function(data_set, file_name){
     
-    data <- read.csv(item, skipNul=TRUE, header=TRUE, stringsAsFactors=FALSE, row.names=NULL)
-    
-    nsites <- nrow(data)
-    cat(paste(data[1,1], ", ", sep=""))
+    nsites <- nrow(data_set)
+    cat(paste(data_set[1,1], ", ", sep=""))
     
     for(col in 3:13){ # indice col for number alternatives
         
         tcost = 0
         
         # time rewrite apps + optimize
-        tRwr = sum(data[,col])*2 #tRr = data[i,3] + tRr
+        tRwr = sum(data_set[,col])*2 #tRr = data[i,3] + tRr
         
         # time test regression
-        ttime = test_time[switch(data[1,1], "barbecue"=1, "jodatime"=2, "commons-lang"=3, "xml-security"=4, "jdepend"=5, "jfreechart"=6)]
-        tReg = (sum(data[,col])+1)*ttime
+        ttime = test_time[switch(data_set[1,1], "barbecue"=1, "jodatime"=2, "commons-lang"=3, "xml-security"=4, "jdepend"=5, "jfreechart"=6)]
+        tReg = (sum(data_set[,col])+1)*ttime
         
         # time LEAP machine + time analyze (sync, filter, totaleu)
-        rep = repetitions[switch(data[1,1], "barbecue"=1, "jodatime"=2, "commons-lang"=3, "xml-security"=4, "jdepend"=5, "jfreechart"=6)]
-        tLeap = ttime*sum(data[,col])*rep*2.6
+        rep = repetitions[switch(data_set[1,1], "barbecue"=1, "jodatime"=2, "commons-lang"=3, "xml-security"=4, "jdepend"=5, "jfreechart"=6)]
+        tLeap = ttime*sum(data_set[,col])*3*rep
         
         # cost [hrs]
         tcost = (tRwr + tReg + tLeap)/3600
@@ -53,6 +49,21 @@ for(item in list.files()){
     }
     
     cat("\n")
+    
+    # TO-DO:write alternatives cost to CSV file
+    
 }
+
+
+# print cost of choosing different percentages of energy efficient alternatives:
+cat(paste("subject, ", "cAll, ", "c100, ", "c90, ", "c80, ", "c70, ", "c60, ", "c50, ", "c40, ", "c30, ", "c20, ", "c10", "\n", sep=""))
+
+for(item in list.files()){
+    
+    data <- read.csv(item, skipNul=TRUE, header=TRUE, stringsAsFactors=FALSE, row.names=NULL)
+    
+    compute_cost(data, "alternatives")
+}
+
 
 
