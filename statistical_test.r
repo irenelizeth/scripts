a080fe44ebd5430fd4cd5da6e4f329d19e587261
irenelizeth <imanotas@udel.edu>
2017-03-14 22:18:03 -0400

    Adding multi-comparison (pot-hoc analysis) for groups with options dunn-test and wilcoxon

--- statistical_test.r
+++ statistical_test.r
@@ -15,6 +15,7 @@ suppressPackageStartupMessages(require(rcompanion))
 suppressPackageStartupMessages(require(multcompView))
 suppressPackageStartupMessages(require(optparse))
 
+
 option_list = list(
     
     make_option(c("-n","--names"), action="store", default=NA, type="character",
@@ -77,18 +78,11 @@ get_result_statistical_test <- function(namesg, samplesg){
 
 }
 
-
-get_results_multicomparison <- function(data, pvalue){
-
-#adjust.method = "BH"
-adjust.method = "none"
-
-#if p-value < alpha then perform multicomparison test:
-if(pvalue < alpha){
+# data: data frame with one column for measurment variable and one column for nominal vble (groups)
+# adjust.method: method used to correct the p-values (familywise error:bonf, or false discovery rate:bh, or none)
+# post-hoc analysis: perform a Wilcoxon (Mann Whitman) Test
+compute_wilcoxon <- function(data, adjust.method){
     
-    #cat(paste("Significant difference found, p-value: ", pvalue, sep=""))
-    cat(pvalue)
-
     data <- mutate(data, Group=factor(data$Group, levels(unique(data$Group))))
     PT <- pairwise.wilcox.test(data$Value, data$Group, p.adjust.method=adjust.method)
     
@@ -98,53 +92,126 @@ if(pvalue < alpha){
     PT <- PT$p.value
     #print(PT)
     PT1 <- fullPTable(PT)
-
+    
     res <- multcompLetters(PT1, compare="<", threshold=alpha, Letters=letters, reversed=FALSE)
     print(res)
-
+    
     #print(res)
     ## groups sharing same character identify groups that are not significantly different
     
     dimMg <- dim(res$LetterMatrix)
     ## if only one column then all groups share same character
-    if(dimMg[2]==1){ 
+    if(dimMg[2]==1){
         cat("\nGroups are not significantly different from each other! \n")
-
+        
     }else if(dimMg[1]>2){ # more than two groups to compare
         cat("\nGroups are significantly different:")
         groupLetters <- res$Letters
         suppressWarnings(suppressMessages(library(sets)))
         setDiffGroups <- set()
         setDiffG <- set()
-
+        
         for(k in 2:length(groupLetters)){
             letter <- as.character(groupLetters[k-1])
             letterk <- as.character(groupLetters[k])
-
+            
             if(!(identical(letter, letterk))){
                 setDiffGroups <- setDiffGroups + set(rownames(res$LetterMatrix)[k-1])
                 setDiffGroups <- setDiffGroups + set(rownames(res$LetterMatrix)[k])
                 #setDiffG <- setDiffG + set(k-1)+ set(k)
             }
         }
-        print(setDiffGroups)
+        return(setDiffGroups)
     }
+
+
+} # end compute_wilcoxon
+
+
+# data: data frame with one column for measurment variable and one column for nominal vble (groups)
+# adjust.method: method used to correct the p-values (familywise error:bonf, or false discovery rate:bh, or none)
+# post-hoc analysis: perform a Dunn Test
+compute_dunntest <- function(data, adjust.method, q){
+
+    suppressWarnings(suppressMessages(library(sets)))
+    suppressWarnings(suppressMessages(library(dunn.test)))
     
-    # post-hoc analysis: perform a Dunn Test
+
     cat("\n\nResults from Dunn Test for multicomparison of groups:\n")
-    PTdt <- dunnTest(data$Value, data$Group, method=adjust.method)
-    print(PTdt)
-    PTdt <- PTdt$res
-    rdt <- cldList(comparison=PTdt$Comparison, p.value = PTdt$P.adj, threshold=alpha)
-    print(rdt)
+    setDiffGroups <- set()
+    
+    # from package FSA
+    #PTdt <- dunnTest(data$Value, data$Group, method=adjust.method, two.sided=FALSE)
+    #print(PTdt)
+    #PTdt <- PTdt$res
+    #rdt <- cldList(comparison=PTdt$Comparison, p.value = PTdt$P.adj, threshold=alpha)
+    
+    # from dunn.test package
+    PT2 <- dunn.test(data$Value, data$Group, method=adjust.method)
+    res <- data.frame(PT2$P, PT2$P.adjusted, PT2$comparisons, stringsAsFactors=FALSE)
+    colnames(res) <- c("P", "P.adj", "test")
+    
+    # TO-DO: check process to accept test based on p.adjusted values
+    # res <- res[order(P),] #sort results based on p.raw.value ?
+    # find largest p.adjusted value for which p.adj < Q ?
+    # accept as significant all tests below largest.p.adjusted < Q ?
+    
+    for(k in 1:nrow(res)){
+        if(adjust.method=="BH" && res$P.adj[k] < q)
+            #cat(res$test[k])
+            setDiffGroups <- setDiffGroups + set(res$test[k])
+        else if (res$P.adj[k] < 0.05)
+            setDiffGroups <- setDiffGroups + set(res$test[k])
+            
+    }
+
+    #TO-DO: sort p.adj-values, find largest p-value that is less than (i/m)*Q, where i is the rank,
+    # m is the total number of comparisons, and Q is the choosen false discovery rate.
+    
+    # BH --> p.adjusted value = p.raw value * (m/i)
+    # sort results by p.unadj or p.raw values (to consider rank position)
+    # find largest p.adjusted value for which p.adj < Q
+    # (test (comparison of groups) is significant if p.adjusted value < Q (false discovery rate))
+    
+    #groups <- as.character(rdt$Group)
+    #groupLetters <- as.character(rdt$Letter)
+    
+    #for(k in 2:length(groups)){
+    #    if(!(identical(groupLetters[k], groupLetters[k-1]))){
+    #        if(!set_contains_element(setDiffGroups, groupLetters[k])){
+    #            setDiffGroups <- setDiffGroups + set(groups[k])
+    #        }
+    #
+    #        if(!set_contains_element(setDiffGroups, groupLetters[k-1])){
+    #            setDiffGroups <- setDiffGroups + set(groups[k-1])
+    #        }
+    #    }
+    #
+    #}
+    
+    return(setDiffGroups)
     cat("End Dunn Test Results\n\n")
-
-#return(setDiffGroups)
-
-
-}else{
-    cat(paste("\n NO significant difference found, p-value ", pvalue,"\n", sep=""))
 }
+
+
+# perform pot-hoc analysis: multicomparison between groups of data
+get_results_multicomparison <- function(data, pvalue){
+
+    #adjust.method = "BH"
+    adjust.method = "BH"
+    # false discovery rate for BH method:
+    q = 0.10
+    
+    #if p-value < alpha then perform multicomparison test:
+    if(pvalue < alpha){
+    
+        #cat(paste("Significant difference found, p-value: ", pvalue, sep=""))
+        cat(pvalue)
+        print(compute_dunntest(data, adjust.method, q))
+
+    }else{
+        cat(paste("\n NO significant difference found, p-value ", pvalue,"\n", sep=""))
+    }
 }# end multicomparison
 
 pvalue <- 1
@@ -152,6 +219,5 @@ pvalue <- 1
 ans <- get_result_statistical_test(list_names, list_samples)
 data <- as.data.frame(ans[1])
 pvalue <- as.double(ans[2])
-
 get_results_multicomparison(data, pvalue)
 
